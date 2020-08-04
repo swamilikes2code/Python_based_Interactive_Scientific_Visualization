161303854fa17bb4e59b341751be3b789a8acfce
cariocabrian <66763061+cariocabrian@users.noreply.github.com>
2020-08-04 13:24:32 -0400

    Update flask_app.py

--- flask_server_setup/flask_app.py
+++ flask_server_setup/flask_app.py
@@ -14,18 +14,25 @@ def index():
 
 @app.route("/sliders_reaction_kinetics", methods=['GET'])
 def sliders_reaction_kinetics():
-    bokeh_script_reaction_kinetics = server_document(url="http://localhost:5006/sliders_reaction_kinetics")
-    return render_template("sliders_reaction_kinetics.html", bokeh_script_reaction_kinetics=bokeh_script_reaction_kinetics)
+    bokeh_script_sliders_reaction_kinetics = server_document(url="http://localhost:5006/sliders_reaction_kinetics")
+    #bokeh_script_reaction_kinetics = server_document(url="https://srrweb.cc.lehigh.edu/sliders_reaction_kinetics")
+    return render_template("sliders_reaction_kinetics.html", bokeh_script_sliders_reaction_kinetics=bokeh_script_sliders_reaction_kinetics)
 
 @app.route("/ZECC", methods=['GET'])
 def ZECC():
     bokeh_script_ZECC = server_document(url="http://localhost:5007/ZECC")
+    #bokeh_script_ZECC = server_document(url="https://srrweb.cc.lehigh.edu/ZECC")
     return render_template("ZECC.html", bokeh_script_ZECC=bokeh_script_ZECC)
 
 @app.route("/SIR", methods=['GET'])
 def SIR():
     bokeh_script_SIR = server_document(url="http://localhost:5005/SIR")
+    #bokeh_script_SIER_Model = server_document(url="https://srrweb.cc.lehigh.edu/SIR")
     return render_template("SIR.html", bokeh_script_SIR=bokeh_script_SIR)
+
+@app.route("/acknowledgements", methods=['GET'])
+def acknowledgements():
+    return render_template("acknowledgements.html")
 
 #run the app
 if __name__ == "__main__":
