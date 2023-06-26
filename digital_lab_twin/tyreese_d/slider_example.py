4fe31ba40ed94e45626407c2b6d29263dc3674a8
TyreeseDavidson <tyreesedavidson00@gmail.com>
2023-06-26 15:30:10 -0400

    attempt at making a slider without custom JS

--- digital_lab_twin/tyreese_d/slider_example.py
+++ digital_lab_twin/tyreese_d/slider_example.py
@@ -3,6 +3,7 @@ from bokeh.plotting import figure, show
 from bokeh.models import ColumnDataSource, CustomJS, Slider
 from bokeh.layouts import column
 from bokeh.io import curdoc
+import numpy as np
 
 # Read data from CSV file
 df = pd.read_csv("PredExperiment.csv")
@@ -20,23 +21,37 @@ p.circle('Time', 'C_X', source=source, size=8)
 slider = Slider(start=0, end=10, value=5, step=0.1, title="Slider")
 
 # Define the callback function
-callback = CustomJS(args=dict(source=source, slider=slider), code="""
-    const data = source.data;
-    const x = data['Time'];
-    const y = data['C_X'];
-    const value = slider.value;
+# callback = CustomJS(args=dict(source=source, slider=slider), code="""
+#     const data = source.data;
+#     const x = data['Time'];
+#     const y = data['C_X'];
+#     const value = slider.value;
 
-    // Update the Y values based on the slider value
-    for (let i = 0; i < y.length; i++) {
-        y[i] = Math.sin(x[i] * value);
-    }
+#     // Update the Y values based on the slider value
+#     for (let i = 0; i < y.length; i++) {
+#         y[i] = Math.sin(x[i] * value);
+#     }
 
-    // Emit the change event to update the plot
-    source.change.emit();
-""")
+#     // Emit the change event to update the plot
+#     source.change.emit();
+# """)
 
-# Attach the callback to the slider
-slider.js_on_change('value', callback)
+# # Attach the callback to the slider
+# slider.js_on_change('value', callback)
+
+def update_data(attr, old, new):
+    a = slider.value
+
+    x = source.data['Time']
+    y1 = source.data['C_X']
+
+    updated_y1 = y1 * a
+
+    source.data = dict(Time=x, C_X=updated_y1)
+
+# Add the callback to the sliders
+slider.on_change('value', update_data)
+
 
 # Create the layout
 layout = column(p, slider)
