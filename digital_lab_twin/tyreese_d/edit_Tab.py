c6e738f12d145b824395bbbde123a6085abbd7f1
TyreeseDavidson <tyreesedavidson00@gmail.com>
2023-06-27 11:33:22 -0400

    adding data and reset button

--- digital_lab_twin/tyreese_d/edit_Tab.py
+++ digital_lab_twin/tyreese_d/edit_Tab.py
@@ -1,22 +1,44 @@
 
 from bokeh.io import show
-from bokeh.models import CustomJS, Select, NumericInput, RadioButtonGroup
+from bokeh.models import CustomJS, Select, NumericInput, RadioButtonGroup, Button, ColumnDataSource
 from bokeh.layouts import column, row
 from bokeh.models.widgets import NumericInput
+import pandas
 
+
+
+# Prediction Data Section ---------------------------------------------------------------------------------------------------------------------
+df = pandas.read_csv("PredExperiment.csv")
+source = ColumnDataSource(df)
+
+#Actual Data Section ---------------------------------------------------------------------------------------------------------------------
+af = pandas.read_csv("ActualExperiment.csv")
+source1 = ColumnDataSource(af)
 #Model Inputs Section-----------------------------------------------------------------------------------------------
 TYPES = ["NN", "GP", "Forest"]
 
-radio_button_group = RadioButtonGroup(name = "Types", labels=TYPES, active=0)
+radio_button_group = RadioButtonGroup(name = "Types", labels=TYPES, active=0)# Student chooses the ML model type
 
-learning_rate = NumericInput(value=0.00001, high = 0.1, low = 0.00001, mode = "float", title="Learning Rate:(0.00001-0.1)")
+learning_rate = NumericInput(value=0.00001, high = 0.1, low = 0.00001, mode = "float", title="Learning Rate:(0.00001-0.1)")# Student chooses the learning rate
 
+optimizer = Select(title="Optimizer:", value="LI", options=["LI", "MSE", "KL Div"])# Student chooses the optimizer
 
-
-optimizer = Select(title="Optimizer:", value="NN", options=["LI", "MSE", "KL Div"])
-loss_Fn = Select(title="Loss Fn:", value="NN", options=["ADAM", "SGD"])
-
+loss_Fn = Select(title="Loss Fn:", value="ADAM", options=["ADAM", "SGD"])# Student chooses the loss function
 
 
+#Rest Buttton Section-----------------------------------------------------------------------------------------------
+reset_button = Button(label = "Reset", button_type = "danger", height = 60, width = 300)
+reset_button.js_on_click(CustomJS(args=dict( lR = learning_rate,  lFn = loss_Fn, opt = optimizer),
+                                  code="""
+   lR.value = 0.00001;
+   lFn.value = "NN";
+    opt.value = "NN";
 
-show(column(radio_button_group,learning_rate, optimizer, loss_Fn))
+
+
+""" ))
+
+
+
+
+show(column(radio_button_group,learning_rate, optimizer, loss_Fn, reset_button))
