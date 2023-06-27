377e0f457efa8b9d82f32050aebc7c0bbf4a92d8
TyreeseDavidson <tyreesedavidson00@gmail.com>
2023-06-27 12:41:15 -0400

    Moved edit tab into the model

--- digital_lab_twin/tyreese_d/bokeh_Module.py
+++ digital_lab_twin/tyreese_d/bokeh_Module.py
@@ -7,7 +7,7 @@ from bokeh.io import curdoc
 from bokeh.layouts import layout
 from bokeh.io import export_svgs
 import datetime
-from bokeh.models import ColumnDataSource, HoverTool, Slider, CustomJS, TabPanel, Tabs, Div, Paragraph, Button  
+from bokeh.models import ColumnDataSource, HoverTool, Slider, CustomJS, TabPanel, Tabs, Div, Paragraph, Button, Select, RadioButtonGroup, NumericInput
 #Intro Text sectionSection ---------------------------------------------------------------------------------------------------------------------
 # NOTE THIS WAS ONLY MADE WITH PYTHON NOT HTML AT ALL
 
@@ -197,7 +197,9 @@ p.legend.background_fill_alpha = 0.5
 p.toolbar.autohide = True
 
 
-# Creating the Button
+# Creating the Button---------------------------------------------------------------------------------------------------------------------
+
+#Reset Button******************************************************************************************************************************
 reset_button = Button(label = "Reset", button_type = "danger", height = 60, width = 300)
 reset_button.js_on_click(CustomJS(args=dict( source = source , li = light_intensity, inf = inlet_flow, pH = pH, inc = inlet_concentration),
                                   code="""
@@ -211,7 +213,7 @@ reset_button.js_on_click(CustomJS(args=dict( source = source , li = light_intens
 
 """ ))
 
-
+#Export Button******************************************************************************************************************************
 # File Export Data Area
 def export_data():
     # Get the data from the ColumnDataSource
@@ -245,10 +247,69 @@ export_button = Button(label="Export Data", button_type="success",  height = 60,
 export_button.on_click(export_data)
 
 
+
+#The Edit Tab ---------------------------------------------------------------------------------------------------------------------
+intro = Div(text="""
+        <h3>Work In Progress!</h3>
+    
+        <h4> Section for bold text</h4>
+    """)
+
+
+# Prediction Data Section ---------------------------------------------------------------------------------------------------------------------
+df = pandas.read_csv("PredExperiment.csv")
+source = ColumnDataSource(df)
+
+#Actual Data Section ---------------------------------------------------------------------------------------------------------------------
+af = pandas.read_csv("ActualExperiment.csv")
+source1 = ColumnDataSource(af)
+
+name = ["Biomass", "Nitrate", "Lutine"]
+p = figure(title = "Change in  concentration over time in a photobioreactor", x_axis_label = "Time(hours)", y_axis_label = "concentration", )
+
+#Actual Lines ******************************************************************************************************************************
+line_a = p.line('Time', 'C_X', source = af, line_width = 4 ,  line_color = "aqua", legend_label = "Biomass")
+p.add_tools(HoverTool(renderers = [line_a], tooltips=[  ('Name', 'Biomass'),
+                                  ('Hour', '@Time'),
+                                  ('Concentration', '@C_X'),# adds the hover tool to the graph for the specifed line
+],))
+
+line_1 = p.line('Time', 'C_X', source = df, line_dash = 'dashdot', line_width = 4, line_color = "blue", legend_label = "Biomass Prediction")
+p.add_tools(HoverTool(renderers = [line_1], tooltips=[  ('Name', 'Biomass Prediction'),
+                                  ('Hour', '@Time'),
+                                  ('Concentration', '@C_X'),# adds the hover tool to the graph for the specifed line
+],))
+#Model Inputs Section-----------------------------------------------------------------------------------------------
+TYPES = ["NN", "GP", "Forest"]
+
+radio_button_group = RadioButtonGroup(name = "Types", labels=TYPES, active=0)# Student chooses the ML model type
+
+learning_rate = NumericInput(value=0.00001, high = 0.1, low = 0.00001, mode = "float", title="Learning Rate:(0.00001-0.1)")# Student chooses the learning rate
+
+optimizer = Select(title="Optimizer:", value="LI", options=["LI", "MSE", "KL Div"], height = 60, width = 300)# Student chooses the optimizer
+
+loss_Fn = Select(title="Loss Fn:", value="ADAM", options=["ADAM", "SGD"], height = 60, width = 300)# Student chooses the loss function
+
+
+#Rest Buttton Section-----------------------------------------------------------------------------------------------
+reset_button = Button(label = "Reset", button_type = "danger", height = 60, width = 300)
+reset_button.js_on_click(CustomJS(args=dict( lR = learning_rate,  lFn = loss_Fn, opt = optimizer),
+                                  code="""
+   lR.value = 0.00001;
+   lFn.value = "NN";
+    opt.value = "NN";
+
+
+
+""" ))
+ls = column( radio_button_group,learning_rate, optimizer, loss_Fn)
+rs = column(p, reset_button)
+bs = row(ls, rs)
 #Making Tabs and showing the Modles ---------------------------------------------------------------------------------------------------------------------
 
 tab1 = TabPanel(child= row(  p,column(reset_button, light_intensity, inlet_flow,inlet_concentration, pH, export_button) ), title="Model")
 tab2 = TabPanel(child = column(intro, info_text, help_text), title = "Instruction")
+tab3 = TabPanel(child = bs, title = "Edit")
 
   
 all_tabs = Tabs(tabs=[tab1, tab2])
