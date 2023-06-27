eb2a4e7814d9ad9c4b6973e749f1fa312a6b4f19
TyreeseDavidson <tyreesedavidson00@gmail.com>
2023-06-27 11:21:47 -0400

    decided setup of the edit tab

--- digital_lab_twin/tyreese_d/edit_Tab.py
+++ digital_lab_twin/tyreese_d/edit_Tab.py
@@ -1,17 +1,22 @@
 
 from bokeh.io import show
-from bokeh.models import CustomJS, Select, Slider, RadioButtonGroup
+from bokeh.models import CustomJS, Select, NumericInput, RadioButtonGroup
 from bokeh.layouts import column, row
+from bokeh.models.widgets import NumericInput
 
-select = Select(title="Type:", value="NN", options=["NN", "GP", "Forest"])
+#Model Inputs Section-----------------------------------------------------------------------------------------------
+TYPES = ["NN", "GP", "Forest"]
 
+radio_button_group = RadioButtonGroup(name = "Types", labels=TYPES, active=0)
 
-slider = Slider(start=0.00001, end=10, value=.1, step=.00001, title="Learning Rate")
-
-LABELS = ["LI", "MSE", "KL Div"]
-
-radio_button_group = RadioButtonGroup(name = "Loss Fn", labels=LABELS, active=0)
-
+learning_rate = NumericInput(value=0.00001, high = 0.1, low = 0.00001, mode = "float", title="Learning Rate:(0.00001-0.1)")
 
 
-show(column(select, slider, radio_button_group))
+
+optimizer = Select(title="Optimizer:", value="NN", options=["LI", "MSE", "KL Div"])
+loss_Fn = Select(title="Loss Fn:", value="NN", options=["ADAM", "SGD"])
+
+
+
+
+show(column(radio_button_group,learning_rate, optimizer, loss_Fn))
