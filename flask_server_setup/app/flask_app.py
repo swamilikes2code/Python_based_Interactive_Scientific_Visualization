93c8f0f1190f367fffa41ad7fe6af5d2dec0c4da
alz425 <135659809+alz425@users.noreply.github.com>
2023-06-29 15:31:22 -0400

    update new file

--- flask_server_setup/app/flask_app.py
+++ flask_server_setup/app/flask_app.py
@@ -1,4 +1,4 @@
-#flask app
+# flask app
 
 from flask import Flask, render_template
 from bokeh.client import pull_session
@@ -6,49 +6,68 @@ from bokeh.embed import server_document
 from flask import send_from_directory
 
 
-#instantiate the flask app
+# instantiate the flask app
 app = Flask(__name__, static_url_path="/app/static")
 
-#create index page function
-@app.route("/app", methods=['GET'])
+
+# create index page function
+@app.route("/app", methods=["GET"])
 def index():
     return render_template("index.html")
 
-@app.route("/sliders_reaction_kinetics", methods=['GET'])
-def sliders_reaction_kinetics():
-    #bokeh_script_sliders_reaction_kinetics = server_document(url="http://localhost:5006/sliders_reaction_kinetics")
-    bokeh_script_sliders_reaction_kinetics = server_document(url="https://srrweb.cc.lehigh.edu/sliders_reaction_kinetics")
-    return render_template("sliders_reaction_kinetics.html", bokeh_script_sliders_reaction_kinetics=bokeh_script_sliders_reaction_kinetics)
 
-@app.route("/ZECC", methods=['GET'])
+@app.route("/sliders_reaction_kinetics", methods=["GET"])
+def sliders_reaction_kinetics():
+    # bokeh_script_sliders_reaction_kinetics = server_document(url="http://localhost:5006/sliders_reaction_kinetics")
+    bokeh_script_sliders_reaction_kinetics = server_document(
+        url="https://srrweb.cc.lehigh.edu/sliders_reaction_kinetics"
+    )
+    return render_template(
+        "sliders_reaction_kinetics.html",
+        bokeh_script_sliders_reaction_kinetics=bokeh_script_sliders_reaction_kinetics,
+    )
+
+
+@app.route("/ZECC", methods=["GET"])
 def ZECC():
-    #bokeh_script_ZECC = server_document(url="http://localhost:5007/ZECC")
+    # bokeh_script_ZECC = server_document(url="http://localhost:5007/ZECC")
     bokeh_script_ZECC = server_document(url="https://srrweb.cc.lehigh.edu/ZECC")
     return render_template("ZECC.html", bokeh_script_ZECC=bokeh_script_ZECC)
 
-@app.route("/SIR", methods=['GET'])
+
+@app.route("/SIR", methods=["GET"])
 def SIR():
-    #bokeh_script_SIR = server_document(url="http://localhost:5008/SIR")
+    # bokeh_script_SIR = server_document(url="http://localhost:5008/SIR")
     bokeh_script_SIR = server_document(url="https://srrweb.cc.lehigh.edu/SIR")
     return render_template("SIR.html", bokeh_script_SIR=bokeh_script_SIR)
 
-@app.route("/catalysis_data_interactive_visualization", methods=['GET'])
-def catalysis_data_interactive_visualization():
-    #bokeh_script_catalysis_data_interactive_visualization = server_document(url="http://localhost:5009/catalysis_data_interactive_visualization")
-    bokeh_script_catalysis_data_interactive_visualization = server_document(url="https://srrweb.cc.lehigh.edu/catalysis_data_interactive_visualization")
-    return render_template("catalysis.html", bokeh_script_catalysis_data_interactive_visualization=bokeh_script_catalysis_data_interactive_visualization)
 
-@app.route("/DAC", methods=['GET'])
+@app.route("/catalysis_data_interactive_visualization", methods=["GET"])
+def catalysis_data_interactive_visualization():
+    # bokeh_script_catalysis_data_interactive_visualization = server_document(url="http://localhost:5009/catalysis_data_interactive_visualization")
+    bokeh_script_catalysis_data_interactive_visualization = server_document(
+        url="https://srrweb.cc.lehigh.edu/catalysis_data_interactive_visualization"
+    )
+    return render_template(
+        "catalysis.html",
+        bokeh_script_catalysis_data_interactive_visualization=bokeh_script_catalysis_data_interactive_visualization,
+    )
+
+
+@app.route("/DAC", methods=["GET"])
 def DAC():
     bokeh_script_DACModel = server_document(url="https://srrweb.cc.lehigh.edu/dac")
-    #bokeh_script_DACModel = server_document(url="http://localhost:5006/DAC_animation")
+    # bokeh_script_DACModel = server_document(url="http://localhost:5006/DAC_animation")
     return render_template("DAC.html", bokeh_script_DACModel=bokeh_script_DACModel)
 
-@app.route("/DLT", methods=['GET'])
+
+@app.route("/DLT", methods=["GET"])
 def DLT():
     bokeh_script_DACModel = server_document(url="https://srrweb.cc.lehigh.edu/DLT")
-    #bokeh_script_DACModel = server_document(url="http://localhost:5006/DAC_animation")
+    # bokeh_script_DACModel = server_document(url="http://localhost:5006/DAC_animation")
     return render_template("DLT.html", bokeh_script_DACModel=bokeh_script_DACModel)
+
+
 # @app.route("/acknowledgements", methods=['GET'])
 # def acknowledgements():
 #     return render_template("acknowledgements.html")
@@ -63,7 +82,6 @@ def DLT():
 #     bokeh_script_DACModel = server_document(url="http://localhost:5006/DAC_animation")
 #     return render_template("DAC.html", bokeh_script_DACModel=bokeh_script_DACModel)
 
-#run the app
+# run the app
 if __name__ == "__main__":
     app.run(port=8080)
- 
