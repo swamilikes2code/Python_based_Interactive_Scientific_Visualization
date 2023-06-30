049960e5881507184c453380c0c65565c1e356e7
Madeline Judd <98533835+krj225@users.noreply.github.com>
2023-06-30 14:48:48 -0400

    my new undertaking: modularizing the NN so users can adjust pieces to their liking

---
+++ digital_lab_twin/maddie-nn/modularNN.py
@@ -0,0 +1,13 @@
+# imports
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import tqdm
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+import joblib
+import pathlib as path
+import copy
