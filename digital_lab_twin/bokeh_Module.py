fe0a2db2f996e0e40b94ecb90805474b6c056fcb
TyreeseDavidson <tyreesedavidson00@gmail.com>
2023-06-30 14:27:14 -0400

    adding preloop code to run button section

--- digital_lab_twin/bokeh_Module.py
+++ digital_lab_twin/bokeh_Module.py
@@ -75,77 +75,7 @@ K_NL = 10.0*1e-3 # g/L  nitrate half- velocity constant for lutein synthesis
 
 
 
-#pytorch  section ---------------------------------------------------------------------------------------------------------------------
-# initialize everything 
-# note that these load funcs will need you to change to your current directory here!
-model = torch.load('models\model.pt')
-model.eval()
-#scalers
-mmscalerX = joblib.load('models\mmscalerX.pkl')
-mmscalerY = joblib.load('models\mmscalerY.pkl')
-
-XTimes = np.linspace(0, 150, 200)
-XDF = pd.DataFrame(XTimes, columns=['Time'])
-#generate empty columns
-XDF['C_X'] = np.zeros(200)
-XDF['C_N'] = np.zeros(200)
-XDF['C_L'] = np.zeros(200)
-XDF['F_in'] = np.zeros(200)
-XDF['C_N_in'] = np.zeros(200)
-XDF['I0'] = np.zeros(200)
-XTimes = XDF.pop('Time') #popped for plotting
-#set initial conditions by changing these vals
-C_X_init = .2
-C_N_init = .2
-C_L_init = 0.0
-F_in_init = 0.001
-C_N_in_init = 10
-I0_init = 150
-
-#function takes in initial conditions and runs the model
-#overwrites XDF with the predicted values
-#updates bokeh plot with new values
-#call when run button is hit
-def predLoop(C_X, C_N, C_L, F_in, C_N_in, I0):
-    #write init conditions to df
-    #Only write to the first row for these 3, they'll be filled in thru the loop
-    XDF['C_X'][0] = C_X
-    XDF['C_N'][0] = C_N
-    XDF['C_L'][0] = C_L
-    #write to all rows for these 3, they won't be filled in thru the loop
-    XDF['F_in'] = F_in
-    XDF['C_N_in'] = C_N_in
-    XDF['I0'] = I0
-
-    #loop through the experiment and predict each timestep
-    for i in range(0, 199):
-        #get the current timestep
-        X_current = XDF.iloc[i]
-        #scale the current timestep
-        X_current_scaled = mmscalerX.transform([X_current])
-        #predict the next timestep
-        Y_current_scaled = model(torch.tensor(X_current_scaled, dtype=torch.float32))
-        #unscale the prediction
-        Y_current_scaled = Y_current_scaled.detach().numpy()
-        Y_current = mmscalerY.inverse_transform(Y_current_scaled)
-        #store the prediction
-        nextTimeStep = i+1
-        XDF.iloc[nextTimeStep, 0] = Y_current[0,0]
-        XDF.iloc[nextTimeStep, 1] = Y_current[0,1]
-        XDF.iloc[nextTimeStep, 2] = Y_current[0,2]
-    #after this loop, XDF should be filled with the predicted values
-    #export XDF to csv
-    #add times back in
-    XDF['Time'] = XTimes
-    XDF.to_csv('outputs\prediction.csv', index=False)
-    #TODO: re-call the plotting function to show results to user
-
-#testing with default values
-predLoop(C_X_init, C_N_init, C_L_init, F_in_init, C_N_in_init, I0_init)
-
-    
-
-
+#pytorch Preloop  section ---------------------------------------------------------------------------------------------------------------------
 
 
 # curdoc().theme = "dark_minimal"# this makes the graph in dark mode
@@ -391,6 +321,75 @@ run_button = Button(label = "Run", button_type = "primary", height = 60, width =
 
 
 # """ ))
+
+# initialize everything 
+# note that these load funcs will need you to change to your current directory here!
+os.chdir('C:\\Users\\kenda\\Documents\\GitHub\\Python_based_Interactive_Scientific_Visualization\\digital_lab_twin\\maddie-nn')
+model = torch.load('models\model.pt')
+model.eval()
+#scalers
+stScalerX = joblib.load('models/stScalerX.pkl')
+stScalerY = joblib.load('models/stScalerY.pkl')
+
+XTimes = np.linspace(0, 150, 200)
+XDF = pd.DataFrame(XTimes, columns=['Time'])
+#generate empty columns
+XDF['C_X'] = np.zeros(200)
+XDF['C_N'] = np.zeros(200)
+XDF['C_L'] = np.zeros(200)
+XDF['F_in'] = np.zeros(200)
+XDF['C_N_in'] = np.zeros(200)
+XDF['I0'] = np.zeros(200)
+XTimes = XDF.pop('Time') #popped for plotting
+#set initial conditions by changing these vals
+C_X_init = .2
+C_N_init = .2
+C_L_init = 0.0
+F_in_init = 0.001
+C_N_in_init = 10
+I0_init = 150
+
+#function takes in initial conditions and runs the model
+#overwrites XDF with the predicted values
+#updates bokeh plot with new values
+#call when run button is hit
+def predLoop(C_X, C_N, C_L, F_in, C_N_in, I0):
+    #write init conditions to df
+    #Only write to the first row for these 3, they'll be filled in thru the loop
+    XDF['C_X'][0] = C_X
+    XDF['C_N'][0] = C_N
+    XDF['C_L'][0] = C_L
+    #write to all rows for these 3, they won't be filled in thru the loop
+    XDF['F_in'] = F_in
+    XDF['C_N_in'] = C_N_in
+    XDF['I0'] = I0
+
+    #loop through the experiment and predict each timestep
+    for i in range(0, 199):
+        #get the current timestep
+        X_current = XDF.iloc[i]
+        #scale the current timestep
+        X_current_scaled = stScalerX.transform([X_current])
+        #predict the next timestep
+        Y_current_scaled = model(torch.tensor(X_current_scaled, dtype=torch.float32))
+        #unscale the prediction
+        Y_current_scaled = Y_current_scaled.detach().numpy()
+        Y_current = stScalerY.inverse_transform(Y_current_scaled)
+        #store the prediction
+        nextTimeStep = i+1
+        XDF.iloc[nextTimeStep, 0] = Y_current[0,0]
+        XDF.iloc[nextTimeStep, 1] = Y_current[0,1]
+        XDF.iloc[nextTimeStep, 2] = Y_current[0,2]
+    #after this loop, XDF should be filled with the predicted values
+    #export XDF to csv
+    #add times back in
+    XDF['Time'] = XTimes
+    XDF.to_csv('outputs\prediction.csv', index=False)
+    #TODO: re-call the plotting function to show results to user
+
+#testing with default values
+#predLoop(C_X_init, C_N_init, C_L_init, F_in_init, C_N_in_init, I0_init)
+
 
 
 #Making Tabs and showing the Modles ---------------------------------------------------------------------------------------------------------------------
