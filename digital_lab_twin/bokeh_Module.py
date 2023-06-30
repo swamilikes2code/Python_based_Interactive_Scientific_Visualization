4ec559551d81b0997d2cabde0e67b75d71ed569a
TyreeseDavidson <tyreesedavidson00@gmail.com>
2023-06-30 15:15:44 -0400

    moving run location, and changed directory

--- digital_lab_twin/bokeh_Module.py
+++ digital_lab_twin/bokeh_Module.py
@@ -77,6 +77,96 @@ K_NL = 10.0*1e-3 # g/L  nitrate half- velocity constant for lutein synthesis
 
 # curdoc().theme = "dark_minimal"# this makes the graph in dark mode
 
+#Creating Sliders ---------------------------------------------------------------------------------------------------------------------
+light_intensity = Slider(start=100, end=200, value=150, step= 1, title="Light Intesity (umol/m2-s):(100 - 200)")
+inlet_flow = Slider(start=0.001, end=0.015, value= 0.008, step=.0001, format = "0.000", title="Inlet Flow(g/L):(0.001 - 0.015)")
+pH = Slider(start=0.1, end=9, value=0.5, step=.1, title="PH")
+inlet_concentration = Slider(start=5, end=15, value=10, step=.1, title="Inlet Concentration(g/L):(5 - 15)")
+nitrate_con = Slider(start=0.2, end=2, value=1, step=.1, title="Initial Nitrate Concentration(g/L):(0.2 - 2)")
+biomass_con = Slider(start=0.2, end=2, value=0.5, step=.1, title="Initial Biomass Concentration(g/L):(0.2 - 2)")
+
+#Run Button******************************************************************************************************************************
+run_button = Button(label = "Run", button_type = "primary", height = 60, width = 300)
+
+def runbutton_function(li = light_intensity, inf = inlet_flow, pH = pH, inc = inlet_concentration, nit = nitrate_con, bio = biomass_con): 
+    
+    #set initial conditions by changing these vals
+    C_X_init = li.value
+    C_N_init = nit.value
+    C_L_init = 0.0
+    F_in_init = inf.value
+    C_N_in_init = inc.value
+    I0_init = li.value
+
+    predLoop(C_X_init, C_N_init, C_L_init, F_in_init, C_N_in_init, I0_init, pH.value, model, stScalerX, stScalerY, source, initial_source)
+
+
+#pytorch Preloop  section ---------------------------------------------------------------------------------------------------------------------
+
+# initialize everything 
+# note that these load funcs will need you to change to your current directory here!
+os.chdir('C:\\Users\\kenda\\Documents\\GitHub\\Python_based_Interactive_Scientific_Visualization\\digital_lab_twin\\nn_and_boekh_setup')
+model = torch.load('models\model.pt')
+model.eval()
+#scalers
+stScalerX = joblib.load('models/stScalerX.pkl')
+stScalerY = joblib.load('models/stScalerY.pkl')
+
+XTimes = np.linspace(0, 150, 200)
+XDF = pd.DataFrame(XTimes, columns=['Time'])
+#generate empty columns
+XDF['C_X'] = np.zeros(200)
+XDF['C_N'] = np.zeros(200)
+XDF['C_L'] = np.zeros(200)
+XDF['F_in'] = np.zeros(200)
+XDF['C_N_in'] = np.zeros(200)
+XDF['I0'] = np.zeros(200)
+XTimes = XDF.pop('Time') #popped for plotting
+
+#function takes in initial conditions and runs the model
+#overwrites XDF with the predicted values
+#updates bokeh plot with new values
+#call when run button is hit
+def predLoop(C_X, C_N, C_L, F_in, C_N_in, I0):
+    #write init conditions to df
+    #Only write to the first row for these 3, they'll be filled in thru the loop
+    XDF['C_X'][0] = C_X
+    XDF['C_N'][0] = C_N
+    XDF['C_L'][0] = C_L
+    #write to all rows for these 3, they won't be filled in thru the loop
+    XDF['F_in'] = F_in
+    XDF['C_N_in'] = C_N_in
+    XDF['I0'] = I0
+
+    #loop through the experiment and predict each timestep
+    for i in range(0, 199):
+        #get the current timestep
+        X_current = XDF.iloc[i]
+        #scale the current timestep
+        X_current_scaled = stScalerX.transform([X_current])
+        #predict the next timestep
+        Y_current_scaled = model(torch.tensor(X_current_scaled, dtype=torch.float32))
+        #unscale the prediction
+        Y_current_scaled = Y_current_scaled.detach().numpy()
+        Y_current = stScalerY.inverse_transform(Y_current_scaled)
+        #store the prediction
+        nextTimeStep = i+1
+        XDF.iloc[nextTimeStep, 0] = Y_current[0,0]
+        XDF.iloc[nextTimeStep, 1] = Y_current[0,1]
+        XDF.iloc[nextTimeStep, 2] = Y_current[0,2]
+    #after this loop, XDF should be filled with the predicted values
+    #export XDF to csv
+    #add times back in
+    XDF['Time'] = XTimes
+    XDF.to_csv('outputs\prediction.csv', index=False)
+    #TODO: re-call the plotting function to show results to user
+
+#testing with default values
+#predLoop(C_X_init, C_N_init, C_L_init, F_in_init, C_N_in_init, I0_init)
+
+
+
+
 #Data Generation Section ---------------------------------------------------------------------------------------------------------------------
 
 data = "outputs\prediction.csv"
@@ -145,12 +235,6 @@ p.toolbar.autohide = True
 
 # Add the Slider to the figure ---------------------------------------------------------------------------------------------------------------------
 
-light_intensity = Slider(start=100, end=200, value=150, step= 1, title="Light Intesity (umol/m2-s):(100 - 200)")
-inlet_flow = Slider(start=0.001, end=0.015, value= 0.008, step=.0001, format = "0.000", title="Inlet Flow(g/L):(0.001 - 0.015)")
-pH = Slider(start=0.1, end=9, value=0.5, step=.1, title="PH")
-inlet_concentration = Slider(start=5, end=15, value=10, step=.1, title="Inlet Concentration(g/L):(5 - 15)")
-nitrate_con = Slider(start=0.2, end=2, value=1, step=.1, title="Initial Nitrate Concentration(g/L):(0.2 - 2)")
-biomass_con = Slider(start=0.2, end=2, value=0.5, step=.1, title="Initial Biomass Concentration(g/L):(0.2 - 2)")
 
 #Define the callback function for the sliders
 def update_data(attr, old, new):
@@ -302,86 +386,6 @@ def export_data():
 
 export_button = Button(label="Export Data", button_type="success",  height = 60, width = 300)
 export_button.on_click(export_data)
-
-#Run Button******************************************************************************************************************************
-run_button = Button(label = "Run", button_type = "primary", height = 60, width = 300)
-
-def runbutton_function(li = light_intensity, inf = inlet_flow, pH = pH, inc = inlet_concentration, nit = nitrate_con, bio = biomass_con): 
-    
-    #set initial conditions by changing these vals
-    C_X_init = li.value
-    C_N_init = nit.value
-    C_L_init = 0.0
-    F_in_init = inf.value
-    C_N_in_init = inc.value
-    I0_init = li.value
-
-    predLoop(C_X_init, C_N_init, C_L_init, F_in_init, C_N_in_init, I0_init, pH.value, model, stScalerX, stScalerY, source, initial_source)
-
-
-#pytorch Preloop  section ---------------------------------------------------------------------------------------------------------------------
-
-# initialize everything 
-# note that these load funcs will need you to change to your current directory here!
-os.chdir('C:\\Users\\kenda\\Documents\\GitHub\\Python_based_Interactive_Scientific_Visualization\\digital_lab_twin\\maddie-nn')
-model = torch.load('models\model.pt')
-model.eval()
-#scalers
-stScalerX = joblib.load('models/stScalerX.pkl')
-stScalerY = joblib.load('models/stScalerY.pkl')
-
-XTimes = np.linspace(0, 150, 200)
-XDF = pd.DataFrame(XTimes, columns=['Time'])
-#generate empty columns
-XDF['C_X'] = np.zeros(200)
-XDF['C_N'] = np.zeros(200)
-XDF['C_L'] = np.zeros(200)
-XDF['F_in'] = np.zeros(200)
-XDF['C_N_in'] = np.zeros(200)
-XDF['I0'] = np.zeros(200)
-XTimes = XDF.pop('Time') #popped for plotting
-
-#function takes in initial conditions and runs the model
-#overwrites XDF with the predicted values
-#updates bokeh plot with new values
-#call when run button is hit
-def predLoop(C_X, C_N, C_L, F_in, C_N_in, I0):
-    #write init conditions to df
-    #Only write to the first row for these 3, they'll be filled in thru the loop
-    XDF['C_X'][0] = C_X
-    XDF['C_N'][0] = C_N
-    XDF['C_L'][0] = C_L
-    #write to all rows for these 3, they won't be filled in thru the loop
-    XDF['F_in'] = F_in
-    XDF['C_N_in'] = C_N_in
-    XDF['I0'] = I0
-
-    #loop through the experiment and predict each timestep
-    for i in range(0, 199):
-        #get the current timestep
-        X_current = XDF.iloc[i]
-        #scale the current timestep
-        X_current_scaled = stScalerX.transform([X_current])
-        #predict the next timestep
-        Y_current_scaled = model(torch.tensor(X_current_scaled, dtype=torch.float32))
-        #unscale the prediction
-        Y_current_scaled = Y_current_scaled.detach().numpy()
-        Y_current = stScalerY.inverse_transform(Y_current_scaled)
-        #store the prediction
-        nextTimeStep = i+1
-        XDF.iloc[nextTimeStep, 0] = Y_current[0,0]
-        XDF.iloc[nextTimeStep, 1] = Y_current[0,1]
-        XDF.iloc[nextTimeStep, 2] = Y_current[0,2]
-    #after this loop, XDF should be filled with the predicted values
-    #export XDF to csv
-    #add times back in
-    XDF['Time'] = XTimes
-    XDF.to_csv('outputs\prediction.csv', index=False)
-    #TODO: re-call the plotting function to show results to user
-
-#testing with default values
-#predLoop(C_X_init, C_N_init, C_L_init, F_in_init, C_N_in_init, I0_init)
-
 
 
 #Making Tabs and showing the Modles ---------------------------------------------------------------------------------------------------------------------
