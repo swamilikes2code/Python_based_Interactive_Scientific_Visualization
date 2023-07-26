fa2c4815833647727b0e59ea3c6d15185e13925e
TyreeseDavidson <tyreesedavidson00@gmail.com>
2023-07-26 11:34:40 -0400

    fixing download_js

--- digital_lab_twin/DLT.py
+++ digital_lab_twin/DLT.py
@@ -317,24 +317,24 @@ download_js = """ function table_to_csv(source) {
         lines.push(row.join(','))
     }
     return lines.join('\n').concat('\n')
-}
+    }
 
 
-const filename = 'data_result.csv'
-const filetext = table_to_csv(source)
-const blob = new Blob([filetext], { type: 'text/csv;charset=utf-8;' })
+    const filename = 'data_result.csv'
+    const filetext = table_to_csv(source)
+    const blob = new Blob([filetext], { type: 'text/csv;charset=utf-8;' })
 
-//addresses IE
-if (navigator.msSaveBlob) {
-    navigator.msSaveBlob(blob, filename)
-} else {
-    const link = document.createElement('a')
-    link.href = URL.createObjectURL(blob)
-    link.download = filename
-    link.target = '_blank'
-    link.style.visibility = 'hidden'
-    link.dispatchEvent(new MouseEvent('click'))
-}"""
+    //addresses IE
+    if (navigator.msSaveBlob) {
+        navigator.msSaveBlob(blob, filename)
+    } else {
+        const link = document.createElement('a')
+        link.href = URL.createObjectURL(blob)
+        link.download = filename
+        link.target = '_blank'
+        link.style.visibility = 'hidden'
+        link.dispatchEvent(new MouseEvent('click'))
+    }"""
 
 
 def export_data():
