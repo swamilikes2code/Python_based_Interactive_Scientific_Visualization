4d4b5da3f224f5ab04b6ce85483c4a56dbadf7b4
Madeline Judd <krj225@lehigh.edu>
2023-10-25 09:58:35 -0400

    pushing back up to 3 layers to understand the impact. generally, this slows things down, from a 3x factor of improvement to 2.3x.

--- digital_lab_twin/modularNN.py
+++ digital_lab_twin/modularNN.py
@@ -58,9 +58,9 @@ def modelCreator(initNeuronNum, loss, optimizer, learnRate):
     model = nn.Sequential(
         nn.Linear(6, initNeuronNum), 
         nn.LeakyReLU(), 
-#nn.Linear(initNeuronNum, (initNeuronNum//2)),
-        #nn.LeakyReLU(),
-        nn.Linear((initNeuronNum), 3))
+        nn.Linear(initNeuronNum, (initNeuronNum//2)),
+        nn.LeakyReLU(),
+        nn.Linear((initNeuronNum//2), 3))
     #define the loss function
     if loss == 0:
         lossFunction = nn.MSELoss()
