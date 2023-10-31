b3ce0f0f6940d7dbda45cd4c80fb3a6b2a679338
Madeline Judd <krj225@lehigh.edu>
2023-10-31 15:34:29 -0400

    HUZZAH. morgan fingerprints for one of the datasets included in the paper. I'm sure I could do this on the other pretty quickly if needed

--- bac/preproccessing.py
+++ bac/preproccessing.py
@@ -10,9 +10,24 @@ import sys
 import time
 import pickle
 import rdkit
+from rdkit import Chem
+from rdkit.Chem import AllChem
 
 # import the data, saved as xlsx files in the data folder
 # for now, lets use the full CF dataset
 cf_data = pd.read_excel('data/CFDataset.xlsx')
-# print out the first few rows to make sure it loaded correctly
-print(cf_data.head())
+
+# for now, I just need the list of smile strings, it's the last column
+smiles_list = cf_data.iloc[:, -1]
+# rdkit doesn't get smiles, it wants mol objs, so we need to convert
+mols_list = [Chem.MolFromSmiles(smile) for smile in smiles_list]
+# note: on a test run, got a 'not removing hydrogen atom without neighbors'
+# warning. come back if this causes problems later
+
+#finally, we need to generate the morgan fingerprints for each molecule
+# this is a list of lists, each list is a fingerprint
+# the first argument is the molecule, the second is the radius of the fingerprint
+# the third is the length of the bit vector
+fp_list = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048) for mol in mols_list]
+# can save this list as a pickle for later use
+pickle.dump(fp_list, open('data/CF_fp_list.pkl', 'wb'))
