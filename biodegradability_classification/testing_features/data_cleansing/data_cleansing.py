3e58e1ac924b8e50b718187a1f30cc432243eacf
slp227 <slp227@lehigh.edu>
2024-06-11 13:33:17 -0400

    Add partial fingerprint code
    Adds fingerprint vector to the csv file

--- biodegradability_classification/testing_features/data_cleansing/data_cleansing.py
+++ biodegradability_classification/testing_features/data_cleansing/data_cleansing.py
@@ -1,12 +1,14 @@
 import pandas as pd
 from rdkit import Chem, RDLogger
 from rdkit.Chem import MACCSkeys
+from rdkit.DataStructs.cDataStructs import ExplicitBitVect
+import numpy as np
 
 # Read the original CSV file into a DataFrame
 df = pd.read_csv("./original_dataset_all.csv")
 
 # Columns to be removed
-columns_to_remove = ['Source', 'CAS Number', 'Name type', 'Index']
+columns_to_remove = ['Source', 'CAS Number', 'Name type', 'Index', 'Smiles']
 
 # Drop the specified columns
 df_cleaned = df.drop(columns=columns_to_remove)
@@ -22,7 +24,13 @@ def smiles_to_molecule(smiles): #function that converts Smiles to RDKit molecule
 # Apply the conversion function to the SMILES column
 RDLogger.DisableLog('rdApp.*') #ignoring hydrogen warning
 df['Molecule'] = df['Smiles'].apply(smiles_to_molecule)
-df['Fingerprint'] = [MACCSkeys.GenMACCSKeys(mol) for mol in df['Molecule']]
+df_cleaned['Fingerprint'] = [MACCSkeys.GenMACCSKeys(mol) for mol in df['Molecule']]
+
+'''
+# creating list representations of the fingerprint vectors
+temp = np.array([list(fp) for fp in df['Fingerprint']]).tolist()
+df_cleaned['Fingerprint'] = np.array([list(fp) for fp in df['Fingerprint']])
+'''
 
 # Save the cleaned DataFrame to a new CSV file
 df_cleaned.to_csv("../biodegrad.csv", index=False)
