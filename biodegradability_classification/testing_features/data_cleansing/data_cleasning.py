6a89c9cfffbdf832f34dc851d78fc7b431b669a8
dep326 <dep326@lehigh.edu>
2024-06-11 12:42:14 -0400

    Added comments for fingerprints adding

--- biodegradability_classification/testing_features/data_cleansing/data_cleasning.py
+++ biodegradability_classification/testing_features/data_cleansing/data_cleasning.py
@@ -4,10 +4,13 @@ import pandas as pd
 df = pd.read_csv("./original_dataset_all.csv")
 
 # Columns to be removed
-columns_to_remove = ['Source', 'CAS Number', 'Name type']
+columns_to_remove = ['Source', 'CAS Number', 'Name type', 'Index']
 
 # Drop the specified columns
 df_cleaned = df.drop(columns=columns_to_remove)
+
+# Create fingerprints here
+# ADD CODE HERE
 
 # Save the cleaned DataFrame to a new CSV file
 df_cleaned.to_csv("../biodegrad.csv", index=False)
