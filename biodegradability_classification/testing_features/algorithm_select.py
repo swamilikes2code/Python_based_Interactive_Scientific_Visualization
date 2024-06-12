8eda940451dceadc9e1606534e834c6456976b42
ecb426 <ecb426@lehigh.edu>
2024-06-12 10:48:14 -0400

    running ML and creating temp_data
    Created temp_data.csv in the testing_features folder, and added the code from data_split_new for the program to run the ML algorithm

--- biodegradability_classification/testing_features/algorithm_select.py
+++ biodegradability_classification/testing_features/algorithm_select.py
@@ -1,23 +1,32 @@
+import pandas as pd
 from bokeh.io import curdoc
-from bokeh.models import CustomJS, Select, Button, Div
+from bokeh.models import Select, Button, Div
+from bokeh.models.callbacks import CustomJS
 from bokeh.layouts import column
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.svm import LinearSVC
-# from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
+
+# load data in from temp_data.csv (this is just a copy of biodegrad.csv, but in the testing_features folder)
+# this is just placeholder, will use data from phase_one.py
+file_path = r'temp_data.csv'
+df = pd.read_csv(file_path)
 
 # creating dropdown menu
-
 global my_alg
 
 # default algorithm
 my_alg = 'DecisionTree'
 
+# Create status message Div
+status_message = Div(text='Not running', styles={'color': 'red', 'font-size': '16px'})
+
 # Select button
 select = Select(title="ML Algorithm:", value="DecisionTree", options=["DecisionTree", "NearestNeighbor", "SVM"])
 
-# Create status message Div
-status_message = Div(text='Not running', styles={'color': 'red', 'font-size': '16px'})
 
 def update_algorithm(attr, old, new):
     global my_alg
@@ -25,12 +34,52 @@ def update_algorithm(attr, old, new):
     status_message.text = 'Not running'
     status_message.styles = {'color': 'red', 'font-size': '16px'}
 
-# Attach callback to the Select widget
+# Attach callback to Select widget
 select.on_change('value', update_algorithm)
+
+# creating widgets
+accuracy_display = Div(text="<div>Validation Accuracy: N/A | Test Accuracy: N/A</div>")
 
 def run_config():
     status_message.text = f'Running {my_alg}'
     status_message.styles = {'color': 'green', 'font-size': '16px'}
+
+    # Assigning model based on selected ML algorithm, using default hyperparameters
+    if my_alg == "DecisionTree":
+        model = DecisionTreeClassifier()
+    elif my_alg == "NearestNeighbor":
+        model = KNeighborsClassifier(n_neighbors=3)
+    else:
+        model = LinearSVC()
+
+    
+    # Was not running properly with fingerprint
+    X = df.drop(columns=['Substance Name', 'Smiles', 'Class', 'Fingerprint'])
+    y = df['Class']
+
+    # function to split data and train model
+    def split_and_train_model(train_percentage, val_percentage, test_percentage):
+
+        # splitting
+        X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=100-train_percentage, random_state=1)
+        test_split = test_percentage / (100-train_percentage)
+        X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=test_split, random_state=1)
+        
+        # train model
+        model.fit(X_train, y_train)
+        
+        # calculating accuracy
+        # FIXME: implement tuning and validation
+        y_val_pred = model.predict(X_val)
+        val_accuracy = accuracy_score(y_val, y_val_pred)
+        y_test_pred = model.predict(X_test)
+        test_accuracy = accuracy_score(y_test, y_test_pred)
+        return f"<div>Training split: {train_percentage}</div><div>Validation split: {val_percentage}</div><div>Testing split: {test_percentage}</div><div><b>Validation Accuracy:</b> {val_accuracy:.2f} | <b>Test Accuracy:</b> {test_accuracy:.2f}</div>"
+
+
+
+    # initializing model training and accuracy display
+    accuracy_display.text = split_and_train_model(50, 25, 25)
 
 # Run button
 run_button = Button(label="Run chosen ML algorithm", button_type="success")
@@ -38,7 +87,6 @@ run_button = Button(label="Run chosen ML algorithm", button_type="success")
 # Attach callback to the run button
 run_button.on_click(run_config)
 
-layout = column(select, run_button, status_message)
-
+layout = column(select, run_button, status_message, accuracy_display)
 
 curdoc().add_root(layout)
