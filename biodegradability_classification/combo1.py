51a5c5cbed78464fd7d2ba732c4d7050cfa4d1ef
slp227 <slp227@lehigh.edu>
2024-06-12 14:02:30 -0400

    Integrate the components into each other
    Has a temporary layout (to be split by tabs later) so it can be viewed using serve for testing purposes

--- biodegradability_classification/combo1.py
+++ biodegradability_classification/combo1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from bokeh.models import ColumnDataSource, DataTable, TableColumn, CheckboxButtonGroup, Button, Div, RangeSlider
+from bokeh.models import ColumnDataSource, DataTable, TableColumn, CheckboxButtonGroup, Button, Div, RangeSlider, Select
 from bokeh.io import curdoc, output_notebook
 from bokeh.layouts import column, row
 from bokeh.models.callbacks import CustomJS
@@ -50,7 +50,7 @@ figure = DataTable(source=source, columns=columns, width=1800)
 checkbox_button_group = CheckboxButtonGroup(labels=optional_columns, active=list(range(len(optional_columns))))
 
 # Create status message Div
-status_message = Div(text='Configuration saved', styles={'color': 'green', 'font-size': '16px'})
+save_status_message = Div(text='Configuration saved', styles={'color': 'green', 'font-size': '16px'})
 
 # Update columns to display
 def update_cols(display_columns):
@@ -62,8 +62,8 @@ def update_cols(display_columns):
 def update(attr, old, new):
     cols_to_display = [checkbox_button_group.labels[i] for i in checkbox_button_group.active]
     update_cols(display_columns=cols_to_display)
-    status_message.text = 'Configuration not saved'
-    status_message.styles = {'color': 'red', 'font-size': '16px'}
+    save_status_message.text = 'Configuration not saved'
+    save_status_message.styles = {'color': 'red', 'font-size': '16px'}
 
 
 
@@ -99,7 +99,7 @@ def split_and_train_model(train_percentage, val_percentage, test_percentage):
 '''
 
 # saved split list to write to
-saved_split_list = [] #0-train, 1-val, 2-test
+saved_split_list = [50,25,25] #0-train, 1-val, 2-test
 
 # helper function to produce string
 def update_text(train_percentage, val_percentage, test_percentage):
@@ -113,8 +113,8 @@ def update_values(attrname, old, new):
     if train_percentage < 10 or val_percentage < 10 or test_percentage < 10:
         return
     # print("train_percentage and temp:", train_percentage, temp, "val and test:", val_percentage, test_percentage)
-    status_message.text = 'Configuration not saved'
-    status_message.styles = {'color': 'red', 'font-size': '16px'}
+    save_status_message.text = 'Configuration not saved'
+    save_status_message.styles = {'color': 'red', 'font-size': '16px'}
     update_text(train_percentage, val_percentage, test_percentage)
     global saved_split_list
     saved_split_list = [train_percentage, val_percentage, test_percentage]
@@ -158,14 +158,15 @@ def save_config():
 
     saved_columns = [checkbox_button_group.labels[i] for i in checkbox_button_group.active]
     # saved_columns = sorted(saved_columns)
+    global saved_col_list
     saved_col_list.clear()
-    saved_col_list.append(saved_columns)
+    saved_col_list = saved_columns
 
     #saved_split_list isn't located here as the split values update in the list upon the change of the range slider
     #the collective save button is to make the design more cohesive
 
-    status_message.text = 'Configuration saved'
-    status_message.styles = {'color': 'green', 'font-size': '16px'}
+    save_status_message.text = 'Configuration saved'
+    save_status_message.styles = {'color': 'green', 'font-size': '16px'}
 
     # print(saved_col_list)
     # print(saved_split_list)
@@ -176,14 +177,96 @@ save_button = Button(label="Save Current Configuration", button_type="success")
 # Attach callback to the save button
 save_button.on_click(save_config)
 
+# --------------- ALGORITHM SELECT ---------------
 
+# algorithm name holder
+my_alg = 'Decision Tree'
 
+# Create status message Div
+alg_status_message = Div(text='Not running', styles={'color': 'red', 'font-size': '16px'})
 
+# Create select button
+select = Select(title="ML Algorithm:", value="Decision Tree", options=["Decision Tree", "K-Nearest Neighbor", "Support Vector Classification"])
 
+def update_algorithm(attr, old, new):
+    global my_alg
+    my_alg = new
+    alg_status_message.text = 'Not running'
+    alg_status_message.styles = {'color': 'red', 'font-size': '16px'}
+
+# Attach callback to Select widget
+select.on_change('value', update_algorithm)
+
+# creating widgets
+accuracy_display = Div(text="<div>Validation Accuracy: N/A | Test Accuracy: N/A</div>")
+# global val_accuracy
+val_accuracy = []
+# global test_accuracy
+test_accuracy = []
+
+def run_config():
+    alg_status_message.text = f'Running {my_alg}'
+    alg_status_message.styles = {'color': 'green', 'font-size': '16px'}
+    global model
+
+    # Assigning model based on selected ML algorithm, using default hyperparameters
+    if my_alg == "Decision Tree":
+        model = DecisionTreeClassifier()
+    elif my_alg == "K-Nearest Neighbor":
+        model = KNeighborsClassifier()
+    else:
+        model = LinearSVC()
+    
+    print(saved_split_list)
+    print(tuple(saved_split_list))
+    print(saved_col_list)
+    accuracy_display.text = split_and_train_model(saved_split_list[0],saved_split_list[1],saved_split_list[2])
+
+    
+
+def split_and_train_model(train_percentage, val_percentage, test_percentage):
+    val_accuracy = []
+    test_accuracy = []
+
+    # run and shuffle five times, and save result in list
+    for i in range(5):
+
+        # Was not running properly with fingerprint
+        # TODO: implement fingerprint decoder so model can read them
+        X = df[saved_col_list]
+        y = df['Class']
+
+        # splitting
+        X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=100-train_percentage)
+        test_split = test_percentage / (100-train_percentage)
+        X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=test_split)
+        
+        # train model
+        model.fit(X_train, y_train)
+        
+        # calculating accuracy
+
+        y_val_pred = model.predict(X_val)
+        val_accuracy.append(accuracy_score(y_val, y_val_pred))
+        y_test_pred = model.predict(X_test)
+        test_accuracy.append(accuracy_score(y_test, y_test_pred))
+
+    return f"<div><b>Validation Accuracy:</b> {str(val_accuracy)} | <b>Test Accuracy:</b> {str(test_accuracy)}</div>"
+
+# Run button
+run_button = Button(label="Run chosen ML algorithm", button_type="success")
+
+# Attach callback to the run button
+run_button.on_click(run_config)
 
 
 # --------------- LAYOUTS ---------------
 
 # creating widget layouts
 table_layout = column(checkbox_button_group, figure)
-slider_layout = column(tvt, split_display, save_button, status_message)
+slider_layout = column(tvt, split_display, save_button, save_status_message)
+tab2_layout = column(select, run_button, alg_status_message, accuracy_display)
+
+# just to see the elements
+test_layout = column(slider_layout, tab2_layout)
+curdoc().add_root(row(test_layout, table_layout))
