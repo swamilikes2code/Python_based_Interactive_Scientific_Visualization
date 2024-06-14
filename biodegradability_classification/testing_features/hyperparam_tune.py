9e9ab449b9fc03e064a62b72d6c59c580f612fe7
slp227 <slp227@lehigh.edu>
2024-06-14 10:53:10 -0400

    Fix and test hyperparameter widgets
    ready for combining

--- biodegradability_classification/testing_features/hyperparam_tune.py
+++ biodegradability_classification/testing_features/hyperparam_tune.py
@@ -1,4 +1,4 @@
-from bokeh.models import Slider, Select
+from bokeh.models import Slider, Select, Checkbox
 from bokeh.layouts import row, column
 from bokeh.io import curdoc
 from sklearn.tree import DecisionTreeClassifier
@@ -6,12 +6,15 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
 
 # global my_alg
-my_alg = 'K-Nearest Neighbors'
+my_alg = 'Support Vector Classification'
 global model
 # model = "test"
 
-slider = Slider()
-select = Select()
+# hyperparameter tuning widgets
+hp_slider = Slider()
+hp_select = Select()
+hp_toggle = Checkbox()
+hp_toggle.margin = (24, 10, 24, 10)
 
 
 if my_alg == 'Decision Tree':
@@ -20,15 +23,58 @@ if my_alg == 'Decision Tree':
     # max_depth of tree (max_depth, int slider)
     model = DecisionTreeClassifier()
 
-    slider.title = "Max Depth of Tree" #can link to the model's hyperparam
-    slider.start= 0
-    slider.end = 15
-    slider.value = 2
-    slider.step = 1
+    # testing purposes
+    def print_vals():
+        print("slider", hp_slider.value)
+        print("switch", hp_toggle.active)
+        print("model", model.max_depth)
+        print("model splitter", model.splitter)
 
-    select.title = "Splitter strategy"
-    select.value = "best"
-    select.options = ["best", "random"]
+    # if switch on, max_depth = None
+    def hp_toggle_callback(attr, old, new):
+        if new == True:
+            hp_slider.update(disabled = True, bar_color = 'black', show_value = False)
+            # hp_slider.disabled = True
+            # hp_slider.bar_color = 'gray'
+            # hp_slider.show_value = False
+            model.max_depth = None
+        elif new == False:
+            hp_slider.update(disabled = False, bar_color = '#e6e6e6', show_value = True)
+            model.max_depth = hp_slider.value
+        print_vals()
+
+    def hp_slider_callback(attr, old, new):
+        if hp_slider.disabled == True:
+            return
+        model.max_depth = new
+        print_vals()
+
+    def hp_select_callback(attr, old, new):
+        model.splitter = new
+        print_vals()
+
+    hp_slider.update(
+        title = "Max Depth of Tree",
+        start= 0,
+        end = 15,
+        value = 2,
+        step = 1
+    )
+    hp_slider.on_change('value', hp_slider_callback)
+
+    hp_toggle.update(
+        label = "None",
+        visible = True,
+        active = False
+    )
+    hp_toggle.on_change('active', hp_toggle_callback)
+
+    hp_select.update(
+        title = "Splitter strategy",
+        value = "best",
+        options = ["best", "random"]
+    )
+    hp_select.on_change('value', hp_select_callback)
 
 elif my_alg == 'K-Nearest Neighbors':
     #hyperparameters are 
@@ -36,40 +82,74 @@ elif my_alg == 'K-Nearest Neighbors':
     # weights (weights, uniform vs. distance, select)
     model = KNeighborsClassifier()
 
-    slider.title = "Number of neighbors"
-    slider.start = 0
-    slider.end = 30
-    slider.value = 5
-    slider.step = 5
+    # testing
+    def print_vals():
+        print("slider", hp_slider.value)
+        print("n_neighbors", model.n_neighbors)
+        print("weights", model.weights)
 
-    select.title = "Weights"
-    select.value = "uniform"
-    select.options = ["uniform", "distance"]
+    def hp_slider_callback(attr, old, new):
+        model.n_neighbors = new
+        print_vals()
+    
+    def hp_select_callback(attr, old, new):
+        model.weights = new
+        print_vals()
 
-elif my_alg == 'Support Vector Machine':
+    hp_slider.update(
+        title = "Number of neighbors",
+        start = 0,
+        end = 30,
+        value = 5,
+        step = 5
+    )
+    hp_slider.on_change('value', hp_slider_callback)
+
+    hp_toggle.visible = False
+
+    hp_select.update(
+        title = "Weights",
+        value = "uniform",
+        options = ["uniform", "distance"]
+    )
+    hp_select.on_change('value', hp_select_callback)
+
+elif my_alg == 'Support Vector Classification':
     #hyperparameters are 
     # loss (loss, hinge vs. squared_hinge, select) 
     # the max iterations to be run (max_iter, int slider)
     model = LinearSVC()
 
-    slider.title = "Maximum iterations"
-    slider.start = 500
-    slider.end = 1500
-    slider.value = 1000
-    slider.step = 100
+    def print_vals():
+        print("slider", hp_slider.value)
+        print("max iter", model.max_iter)
+        print("loss func", model.loss)
 
-    select.title = "Loss function"
-    select.value = "squared_hinge"
-    select.options = ["squared_hinge", "hinge"]
+    def hp_slider_callback(attr, old, new):
+        model.max_iter = new
+        print_vals()
+    
+    def hp_select_callback(attr, old, new):
+        model.loss = new
+        print_vals()
 
+    hp_slider.update(
+        title = "Maximum iterations", #default is 1000
+        start = 500,
+        end = 1500,
+        value = 1000,
+        step = 100
+    )
+    hp_slider.on_change('value', hp_slider_callback)
 
-# for checking that assignment worked
+    hp_toggle.visible = False
 
-# def cb(attr, old, new):
-#     print(new)
-#     print(model)
+    hp_select.update(
+        title = "Loss function",
+        value = "squared_hinge",
+        options = ["squared_hinge", "hinge"]
+    )
+    hp_select.on_change('value', hp_select_callback)
 
-# slider.on_change('value', cb)
-
-l = column(slider, select)
+l = column(row(hp_slider, hp_toggle), hp_select)
 curdoc().add_root(l)
