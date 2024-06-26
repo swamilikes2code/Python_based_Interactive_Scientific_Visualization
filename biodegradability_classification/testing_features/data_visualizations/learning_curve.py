81608a591cad56cedea49632b66bfe535be4790c
dep326 <dep326@lehigh.edu>
2024-06-26 13:50:34 -0400

    Visual Enhancements to Train tab

--- biodegradability_classification/testing_features/data_visualizations/learning_curve.py
+++ biodegradability_classification/testing_features/data_visualizations/learning_curve.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from bokeh.plotting import figure, show, output_file
 from bokeh.models import ColumnDataSource, HoverTool
@@ -9,12 +10,12 @@ from bokeh.models import ColumnDataSource, HoverTool
 df = pd.read_csv('../rdkit_test.csv') # Load dataset
 
 
-features = df[['MolWt', 'NumValenceElectrons', 'NumRadicalElectrons']] #independent variables / features 
+features = df[['MolWt', 'NumValenceElectrons', 'NumRadicalElectrons', 'HeavyAtomMolWt','ExactMolWt']] #independent variables / features 
 target = df['Class'] #target variable
 
 X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42) # split data into training and testing sets
 
-train_sizes = np.linspace(0.1, 1.0, 10) # Defines array from 0.1 to 1.0 in 10 evenly spaced out increments. The result is 10 numbers are generated... 0.1, 0.2, 0.3 ...  to 1.0
+train_sizes = np.linspace(0.1, 1.0, 15) # Defines array from 0.1 to 1.0 in 10 evenly spaced out increments. The result is 10 numbers are generated... 0.1, 0.2, 0.3 ...  to 1.0
 
 train_scores = [] # allocate space on machine to hold the test and train results to later plot on the learning curve
 test_scores = []
@@ -24,7 +25,7 @@ for size in train_sizes: #for each size in the array train_size
     X_train_subset = X_train.sample(frac=size, random_state=42) #from X_train (which holds 80% of the rows from the original dataset), take frac % of the rows from there to get a smaller percentage. state = 42 for reproduciblity
     y_train_subset = y_train.loc[X_train_subset.index] #X_train_subset.index gives you the index of the points taken in the line above. Then it extracts the corresponding class results for the smaller subset taken above so we can calculate the accuracy
     
-    model = DecisionTreeClassifier() # Train the model with new subsets
+    model = KNeighborsClassifier(n_neighbors=1) # Train the model with new subsets
     model.fit(X_train_subset, y_train_subset)
     
     train_score = accuracy_score(y_train_subset, model.predict(X_train_subset)) # Evaluate on training and test sets
@@ -51,6 +52,7 @@ p = figure(
     x_axis_label='Training Size (Fraction)', 
     y_axis_label='Accuracy',
     tools='pan, wheel_zoom, box_zoom, reset',
+    x_range=(0, 1),
     y_range=(0, 1)  # Set y-axis range from 0 to 1
 )
 
