5e1f7a9870beee2294a017618b44c918a41bc169
slp227 <slp227@lehigh.edu>
2024-06-28 21:59:27 -0400

    Fixing option file for base py file to work again

--- biodegradability_classification/testing_features/data_cleansing/new_data_cleansing.py
+++ biodegradability_classification/testing_features/data_cleansing/new_data_cleansing.py
@@ -5,15 +5,16 @@ from rdkit.Chem import MACCSkeys, DataStructs, Descriptors, AllChem
 import numpy as np
 
 # Read the original CSV file into a DataFrame
-df = pd.read_csv("./original_dataset_all.csv")
+df_og = pd.read_csv("./original_dataset_all.csv")
 
 # only keep three identifying cols
-df = df.loc[:, ['Substance Name', 'Smiles', 'Class']]
+df = df_og.loc[:, ['Substance Name', 'Smiles', 'Class']]
 # print(df)
 
 # dropping data from the non-biodegradable class to balance the data
 df = df.drop(df[df['Class'] == 0].sample(frac = .46).index)
 df.reset_index(drop=True, inplace=True)
+mandatory = df.copy()
 # print(df)
 
 # making molecules
@@ -27,13 +28,14 @@ def smiles_to_molecule(smiles): #function that converts Smiles to RDKit molecule
 # Apply the conversion function to the SMILES column
 RDLogger.DisableLog('rdApp.*') #ignoring hydrogen warning
 molecs = df['Smiles'].apply(smiles_to_molecule)
-print(df.shape)
+# print(df.shape)
 # print(df)
 
 # generating descriptors (using all descriptors, can specify list to generate)
 desc = [Descriptors.CalcMolDescriptors(mol) for mol in molecs]
 desc_df = pd.DataFrame(desc)
 option_1 = desc_df.drop(columns=['MaxPartialCharge', 'MaxAbsPartialCharge', 'Ipc', 'MinPartialCharge', 'MinAbsPartialCharge', 'BCUT2D_MWHI', 'BCUT2D_MWLOW', 'BCUT2D_CHGHI', 'BCUT2D_CHGLO', 'BCUT2D_LOGPHI', 'BCUT2D_LOGPLOW', 'BCUT2D_MRHI', 'BCUT2D_MRLOW'])
+option_1 = pd.concat([mandatory, option_1], axis=1)
 # print(desc_df.isnull().any().any())
 # print(desc_df)
 
@@ -75,16 +77,19 @@ pathfp_vec = [molecule_to_pathfp(mol) for mol in molecs]
 mfp_array = np.stack(mfp_vec, axis=0)
 option_2 = pd.DataFrame(mfp_array)
 option_2 = option_2.astype('int8')
+option_2 = pd.concat([mandatory, option_2], axis=1)
 df = pd.concat([df, option_2], axis=1)
 
 ECFP_array = np.stack(ECFP_vec, axis=0)
 option_3 = pd.DataFrame(ECFP_array)
 option_3 = option_3.astype('int8')
+option_3 = pd.concat([mandatory, option_3], axis=1)
 df = pd.concat([df, option_3], axis=1)
 
 pathfp_array = np.stack(pathfp_vec, axis=0)
 option_4 = pd.DataFrame(pathfp_array)
 option_4 = option_4.astype('int8')
+option_4 = pd.concat([mandatory, option_4], axis=1)
 df = pd.concat([df, option_4], axis=1)
 
 print(option_1.shape)
@@ -112,7 +117,7 @@ print(df.shape)
 # Save the cleaned DataFrame to a new CSV file
 # df.to_csv("../../data/all.csv", index=False)
 # df.to_csv("../../data/mandatory.csv", index=False)
-# option_1.to_csv("../../data/option_1.csv", index=False)
-# option_2.to_csv("../../data/option_2.csv", index=False)
-# option_3.to_csv("../../data/option_3.csv", index=False)
-# option_4.to_csv("../../data/option_4.csv", index=False)
+option_1.to_csv("../../data/option_1.csv", index=False)
+option_2.to_csv("../../data/option_2.csv", index=False)
+option_3.to_csv("../../data/option_3.csv", index=False)
+option_4.to_csv("../../data/option_4.csv", index=False)
