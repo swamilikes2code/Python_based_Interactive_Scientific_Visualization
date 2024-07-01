a2b63276263730af29584a86cf3e9f6e18fed458
dep326 <dep326@lehigh.edu>
2024-07-01 09:49:02 -0400

    learning curve iloc add

--- biodegradability_classification/testing_features/data_visualizations/learning_curve.py
+++ biodegradability_classification/testing_features/data_visualizations/learning_curve.py
@@ -7,15 +7,15 @@ from sklearn.metrics import accuracy_score
 from bokeh.plotting import figure, show, output_file
 from bokeh.models import ColumnDataSource, HoverTool
 
-df = pd.read_csv('../rdkit_test.csv') # Load dataset
+df = pd.read_csv('../../data/option_1.csv') # Load dataset
 
 
-features = df[['MolWt', 'NumValenceElectrons', 'NumRadicalElectrons', 'HeavyAtomMolWt','ExactMolWt']] #independent variables / features 
+features = df.iloc[:,3:] #independent variables / features 
 target = df['Class'] #target variable
 
 X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42) # split data into training and testing sets
 
-train_sizes = np.linspace(0.1, 1.0, 15) # Defines array from 0.1 to 1.0 in 10 evenly spaced out increments. The result is 10 numbers are generated... 0.1, 0.2, 0.3 ...  to 1.0
+train_sizes = np.linspace(0.01, 1.0, 30) # Defines array from 0.1 to 1.0 in 10 evenly spaced out increments. The result is 10 numbers are generated... 0.1, 0.2, 0.3 ...  to 1.0
 
 train_scores = [] # allocate space on machine to hold the test and train results to later plot on the learning curve
 test_scores = []
@@ -25,7 +25,7 @@ for size in train_sizes: #for each size in the array train_size
     X_train_subset = X_train.sample(frac=size, random_state=42) #from X_train (which holds 80% of the rows from the original dataset), take frac % of the rows from there to get a smaller percentage. state = 42 for reproduciblity
     y_train_subset = y_train.loc[X_train_subset.index] #X_train_subset.index gives you the index of the points taken in the line above. Then it extracts the corresponding class results for the smaller subset taken above so we can calculate the accuracy
     
-    model = KNeighborsClassifier(n_neighbors=1) # Train the model with new subsets
+    model = KNeighborsClassifier(n_neighbors=5) # Train the model with new subsets
     model.fit(X_train_subset, y_train_subset)
     
     train_score = accuracy_score(y_train_subset, model.predict(X_train_subset)) # Evaluate on training and test sets
@@ -36,6 +36,11 @@ for size in train_sizes: #for each size in the array train_size
 
 train_scores = np.array(train_scores) # Convert lists to numpy arrays
 test_scores = np.array(test_scores)
+
+# Manually prepend (0,0) to ensure the curve starts at the baseline
+train_sizes = np.insert(train_sizes, 0, 0)
+train_scores = np.insert(train_scores, 0, 0)
+test_scores = np.insert(test_scores, 0, 0)
 
 output_file('learning_curve_custom_split.html') # Create a Bokeh plot
 
