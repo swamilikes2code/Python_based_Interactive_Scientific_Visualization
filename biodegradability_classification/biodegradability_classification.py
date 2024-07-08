58e5c9ffa715d1ee61b1affd43a94fa5aeac761d
slp227 <slp227@lehigh.edu>
2024-07-08 09:58:07 -0400

    Fix the model list
    wasn't working in test tab because not all models were being saved

--- biodegradability_classification/biodegradability_classification.py
+++ biodegradability_classification/biodegradability_classification.py
@@ -926,7 +926,6 @@ def run_ML():
     train_status_message.styles = updated
     # set_hyperparameter_widgets()
     train_validate_model()
-    model_list.append(model)
 
 def split_data(train_percentage, val_percentage, test_percentage, data_index):
     global X_train, X_val, X_test, y_train, y_val, y_test
@@ -978,6 +977,7 @@ def train_validate_model():
 
     set_learning_curve()
     save_model()
+    model_list.append(model)
 
 def load_ML():
     train_status_message.text = f'Running {my_alg}...'
