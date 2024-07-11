587fa3e46b17b78ad95b5cac90fa1d9d480770d2
slp227 <slp227@lehigh.edu>
2024-07-11 10:22:05 -0400

    Add histogram to module

--- biodegradability_classification/biodegradability_classification.py
+++ biodegradability_classification/biodegradability_classification.py
@@ -12,6 +12,7 @@ from bokeh.models.callbacks import CustomJS
 from bokeh.models.dom import HTML
 from bokeh.models.ui import SVGIcon
 from bokeh.plotting import figure
+from bokeh.transform import dodge
 import pubchempy
 from rdkit import Chem, RDLogger
 from rdkit.Chem import DataStructs, Descriptors, AllChem
@@ -884,8 +885,135 @@ def load_config():
 # Attach callback to the save button
 save_config_button.on_click(load_config)
 
+#############################################
+# --------------- HISTOGRAM --------------- #
+#############################################
+
+# Split data based on the class
+class_0 = df1[df1['Class'] == 0]
+class_1 = df1[df1['Class'] == 1]
+
+# print(class_0.shape)
+# print(class_1.shape)
+
+# Default histogram column
+default_hist_column = 'Class'
+
+# Define the bins
+bins = np.linspace(df1[default_hist_column].min(), df1[default_hist_column].max(), 20)
+
+# Calculate histogram for each class
+hist_0, edges_0 = np.histogram(class_0[default_hist_column], bins=bins)
+hist_1, edges_1 = np.histogram(class_1[default_hist_column], bins=bins)
+
+# Prepare data for plotting
+
+# Calculate the center positions of each bin
+hist_centers = (edges_0[:-1] + edges_0[1:]) / 2
+hist_width = .3*(hist_centers[1]-hist_centers[0])
+dodge_val = .625*hist_width
+
+# Create a new ColumnDataSource
+source = ColumnDataSource(data=dict(
+    hist_centers=hist_centers,
+    top_class_0=hist_0,
+    top_class_1=hist_1
+))
+
+# Create the figure
+histogram = figure(title=f"Histogram of {default_hist_column} with Class Color Coding",
+           x_axis_label=default_hist_column, y_axis_label='Frequency',
+           tools="save",
+           width=800, height=400)
+
+# Add class 0 bars
+bars_class_0 = histogram.vbar(x=dodge('hist_centers', -0.15, range=histogram.x_range), top='top_class_0', width=0.3*(hist_centers[1] - hist_centers[0]),
+                      color='blue', alpha=0.6, legend_label='Class 0', source=source)
+
+# Add class 1 bars
+bars_class_1 = histogram.vbar(x=dodge('hist_centers', 0.15, range=histogram.x_range), top='top_class_1', width=0.3*(hist_centers[1] - hist_centers[0]),
+                      color='red', alpha=0.6, legend_label='Class 1', source=source)
+
+# Add hover tool for interaction
+hover = HoverTool()
+hover.tooltips = [("Range", "@hist_centers"),
+                  ("Class 0 Frequency", "@top_class_0"),
+                  ("Class 1 Frequency", "@top_class_1")]
+histogram.add_tools(hover)
+
+# Style the plot
+histogram.legend.click_policy = "hide"
+histogram.legend.location = "top_right"
+histogram.xgrid.grid_line_color = None
+histogram.ygrid.grid_line_color = "gray"
+histogram.ygrid.grid_line_dash = [6, 4]
+
+# Create a Select widget for choosing histogram column
+# hist_options = df1.columns[2:16].tolist() # CHANGE THIS LINE FOR THE OPTIONS. 
+hist_options = ['Class',
+                'MolWt',
+                'NumValenceElectrons',
+                'NumRadicalElectrons',
+                'HeavyAtomCount',
+                'NHOHCount',
+                'NOCount',
+                'NumAliphaticCarbocycles',
+                'NumAliphaticHeterocycles',
+                'NumAliphaticRings',
+                'NumAromaticCarbocycles',
+                'NumAromaticHeterocycles',
+                'NumAromaticRings',
+                'NumHAcceptors',
+                'NumHDonors',
+                'NumHeteroatoms',
+                'NumRotatableBonds',
+                'NumSaturatedCarbocycles',
+                'NumSaturatedHeterocycles',
+                'NumSaturatedRings',
+                'RingCount',
+                ]
+hist_x_select = Select(title="X Axis:", value=default_hist_column, options=hist_options)
+
+# Callback function for Select widget
+def update_hist(attrname, old, new):
+    selected_column = hist_x_select.value
+
+    bins = np.linspace(df1[selected_column].min(), df1[selected_column].max(), 20)
+
+    # Update histogram data based on selected column
+    hist_0, edges_0 = np.histogram(class_0[selected_column], bins=bins)
+    hist_1, edges_1 = np.histogram(class_1[selected_column], bins=bins)
+
+    hist_centers = (edges_0[:-1] + edges_0[1:]) / 2
+    hist_width = .3*(hist_centers[1]-hist_centers[0])
+    dodge_val = .625*hist_width
+
+    source.data = dict(
+        hist_centers=hist_centers,
+        top_class_0=hist_0,
+        top_class_1=hist_1
+    )
+    bars_class_0.data_source.data['top'] = hist_0
+    bars_class_1.data_source.data['top'] = hist_1
+
+    bars_class_0.glyph.update(
+        x=dodge('hist_centers', -dodge_val, range=histogram.x_range),
+        width=hist_width
+    )
+    bars_class_1.glyph.update(
+        x=dodge('hist_centers', dodge_val, range=histogram.x_range),
+        width=hist_width
+    )
+
+    histogram.title.text = f"Histogram of {selected_column} with Class Color Coding"
+    histogram.xaxis.axis_label = selected_column
+
+# Attach callback to Select widget
+hist_x_select.on_change('value', update_hist)
+
+
 ######################################################
-# --------------- Learning Curve Code ---------------# 
+# --------------- LEARNING CURVE ---------------# 
 ######################################################
 
 sizes = np.linspace(.01, 1.0, 15)
@@ -2090,7 +2218,7 @@ data_config_layout = layout(
     [column(save_config_button, save_config_message)]
 )
 
-tab1_layout = row(left_page_spacer, column(top_page_spacer, row(column(step_one, data_config_layout), data_tab_table), tiny_height_spacer))
+tab1_layout = row(left_page_spacer, column(top_page_spacer, row(column(step_one, data_config_layout), data_tab_table), tiny_height_spacer, hist_x_select, histogram))
 
 hyperparam_layout = layout(
     [step_three],
