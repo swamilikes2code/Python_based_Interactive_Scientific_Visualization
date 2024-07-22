7998b63ac023b49489b00c1eea4f8f05ebd5b900
slp227 <slp227@lehigh.edu>
2024-07-22 11:15:47 -0400

    Change tune to validate for consistency

--- biodegradability_classification/biodegradability_classification.py
+++ biodegradability_classification/biodegradability_classification.py
@@ -64,7 +64,7 @@ delete_status_message = Div(text='Changes not saved', styles = not_updated)
 
 save_config_button = Button(label="Save Current Configuration", button_type="warning", width = 250)
 train_button = Button(label="Train", button_type="success", width=150, height = 31)
-tune_button = Button(label="Tune", button_type="success", width=150, height = 31)
+tune_button = Button(label="Validate", button_type="success", width=150, height = 31)
 delete_button = Button(label = "Delete", button_type = 'danger', width = 200, height = 31)
 test_button = Button(label = "Test", button_type = "success", width = 150, height = 31)
 predict_button = Button(label = 'Predict', button_type = "success", width = 200, height = 31)
