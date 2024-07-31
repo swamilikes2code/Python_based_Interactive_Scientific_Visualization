7aa4a0ba5bcf69b41a26b2aa19394bc9d6f9de73
ecb426 <ecb426@lehigh.edu>
2024-07-31 10:23:33 -0400

    mobile-friendly reaction kinetics bokeh

--- reaction_kinetics/sliders_reaction_kinetics.py
+++ reaction_kinetics/sliders_reaction_kinetics.py
@@ -69,7 +69,7 @@ source_vbar = ColumnDataSource(data=dict(specie_names=specie_names, vbar_top=vba
 TOOLTIPS = [("Time (s)","@vec_time"), ("A","@int_vec_A{0,0.000}"), ("B","@int_vec_B{0,0.000}"), ("C","@int_vec_C{0,0.000}")]
 TOOLS = "undo,redo,reset,save,box_zoom"
 # original height: 450, width: 550
-plot_conc = figure(height=300, width=380, tools=TOOLS, tooltips=TOOLTIPS,
+plot_conc = figure(height=400, width=450, tools=TOOLS, tooltips=TOOLTIPS,
               title="Sequential reactions involving A, B and C", x_range=[t_start, t_end], y_range=[-0.05, 1.05])
 plot_conc.line('vec_time', 'int_vec_A', source=source, line_width=3, line_alpha=0.6, line_color="darkgray",
                legend_label="A Concentration")
@@ -86,8 +86,8 @@ plot_conc.grid.grid_line_color = "silver"
 
 # Set up vertical bar plot for concentrations at a certain time
 TOOLTIPS_vbar = [("Specie_Name","@specie_names"), ("Concentration","@vbar_top{0,0.000}")]
-plot_vbar = figure(height=300, width=380, tools=TOOLS, tooltips=TOOLTIPS_vbar, x_range=specie_names,
-                   y_range=[-0.05, 1.05], title="Concentration A, B and C at time specified by time slider")
+plot_vbar = figure(height=400, width=450, tools=TOOLS, tooltips=TOOLTIPS_vbar, x_range=specie_names,
+                   y_range=[-0.05, 1.05], title="Concentration A, B and C at time specified by slider")
 plot_vbar.vbar(x='specie_names', top='vbar_top', source=source_vbar, bottom=0.0, width=0.5, alpha=0.6, color="color",
                legend_field="specie_names")
 plot_vbar.xaxis.axis_label = "Species"
@@ -162,7 +162,7 @@ left_page_spacer = Spacer(width = 20)
 # inputs_time = column(animate_button, slider_time)
 
 inputs_reaction = row(left_page_spacer, column(top_page_spacer, text, slider_k_AB, slider_k_BC, slider_order_AB, slider_order_BC))
-inputs_time = column(animate_button, slider_time)
+inputs_time = row(left_page_spacer, column(animate_button, slider_time))
 
 tab1 =TabPanel(child=row(inputs_reaction, plot_conc, column(plot_vbar, inputs_time)), title="Reaction Kinetics")
 
