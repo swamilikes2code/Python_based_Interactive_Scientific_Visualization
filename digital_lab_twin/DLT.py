60fccc3bb3aaae97fb734b1c80bb8f52b48c6b7a
ecb426 <ecb426@lehigh.edu>
2024-08-01 10:22:38 -0400

    fix callbacks (specify for each tab) DLT

--- digital_lab_twin/DLT.py
+++ digital_lab_twin/DLT.py
@@ -52,11 +52,13 @@ updated = {'color': 'green', 'font-size': '14px'}
 run_button_status_message = Div(text='Not running', styles=not_updated)
 run_button_edit_tab_status_message = Div(text='Not running', styles=not_updated)
 
-def status_messages_callback(attr, old, new):
-    run_button_status_message.text = 'Not running'
-    run_button_status_message.styles = not_updated
+def train_status_message_callback(attr, old, new):
     run_button_edit_tab_status_message.text = 'Not running'
     run_button_edit_tab_status_message.styles = not_updated
+
+def optimize_status_message_callback(attr, old, new):
+    run_button_status_message.text = 'Not running'
+    run_button_status_message.styles = not_updated
 
 intro = Div(text="""
        
@@ -139,11 +141,11 @@ inlet_flow = Slider(start=0.001, end=0.015, value= 0.008, step=.0001, format = "
 inlet_concentration = Slider(start=5, end=15, value=10, step=.1, title="Inlet Concentration of Nitrates(g/L):(5 - 15)")
 nitrate_con = Slider(start=0.2, end=2, value=1, step=.05, title="Initial Nitrate Concentration(g/L):(0.2 - 2)")
 biomass_con = Slider(start=0.2, end=2, value=0.5, step=.05, title="Initial Biomass Concentration(g/L):(0.2 - 2)")
-light_intensity.on_change('value', status_messages_callback)
-inlet_flow.on_change('value', status_messages_callback)
-inlet_concentration.on_change('value', status_messages_callback)
-nitrate_con.on_change('value', status_messages_callback)
-biomass_con.on_change('value', status_messages_callback)
+light_intensity.on_change('value', optimize_status_message_callback)
+inlet_flow.on_change('value', optimize_status_message_callback)
+inlet_concentration.on_change('value', optimize_status_message_callback)
+nitrate_con.on_change('value', optimize_status_message_callback)
+biomass_con.on_change('value', optimize_status_message_callback)
 
 
 #pytorch Preloop  section ---------------------------------------------------------------------------------------------------------------------
@@ -417,40 +419,40 @@ loss_options = ["MSE", "MAE"]
 # changed direction of helpbuttons from right to left (better when viewed in portrait mode)
 
 train = NumericInput(value=0.6, high = 0.7, low = 0.1, mode = "float", title="Train Split:(0.1-0.7)", )# 
-train.on_change('value', status_messages_callback)
+train.on_change('value', train_status_message_callback)
 train_tooltip = Tooltip(content=("""Determine as a percentage how much of the data will be used to teach the model. What is left out of training will be used to validate and test."""), position = "left")
 train_help_button = HelpButton(tooltip=train_tooltip, button_type = "light", )
 
 #val_split = NumericInput(value=0.2, high = 100, low = 0, mode = "float", title="Val Split:(0 - 1)")# 
 
 neurons = Slider (start = 7, end = 50, value = 18, step = 1, title = "Number of Neurons")# 
-neurons.on_change('value', status_messages_callback)
+neurons.on_change('value', train_status_message_callback)
 neurons_tooltip = Tooltip(content=("""Determine how dense each neural network layer is. The network contains 3 layers, with an activator function in between each. Denser networks are resource intensive, but thinner networks may compromise accuracy."""), position = "left")
 neurons_help_button = HelpButton(tooltip=neurons_tooltip, button_type = "light")
 
 epochs = Slider (start = 5, end = 30, value = 25, step = 5, title = "Epochs")# 
-epochs.on_change('value', status_messages_callback)
+epochs.on_change('value', train_status_message_callback)
 epochs_tooltip = Tooltip(content=("""Determine how many times the network will read over the training data. This heavily impacts the model’s processing time."""), position = "left")
 epochs_help_button = HelpButton(tooltip=epochs_tooltip, button_type = "light")
     
 batch_Size = Slider (start = 25, end = 200, value = 25, step = 25, title = "Batch Size")# 
-batch_Size.on_change('value', status_messages_callback)
+batch_Size.on_change('value', train_status_message_callback)
 batch_Size_tooltip = Tooltip(content=("""Determine how many datapoints to feed the network at one time. An ideal batch size will help optimize runtime and model accuracy."""), position = "left")
 batch_Size_help_button = HelpButton(tooltip=batch_Size_tooltip, button_type = "light")
 
 
 learning_rate = NumericInput(value=0.001, high = 0.01, low = 0.0001, mode = "float", title="Learning Rate:(0.0001-0.01)")# Student chooses the learning rate
-learning_rate.on_change('value', status_messages_callback)
+learning_rate.on_change('value', train_status_message_callback)
 learning_rate_tooltip = Tooltip(content=("""Choose a maximum value by which the optimizer may adjust neuron weights. The lower this is, the smaller the changes any given epoch will have on the model."""), position = "left")
 learning_rate_help_button = HelpButton(tooltip=learning_rate_tooltip, button_type = "light")
 
 loss_Fn = Select(title="Loss Function:", value="MAE", options= loss_options, height = 60, width = 300)# Student chooses the loss function
-loss_Fn.on_change('value', status_messages_callback)
+loss_Fn.on_change('value', train_status_message_callback)
 loss_Fn_tooltip = Tooltip(content=f"Choose an algorithm to measure the accuracy of your predictions. MSE judges by square loss, whereas MAE judges by absolute loss. ", position = "left") #{', '.join(loss_options)}
 loss_Fn_help_button = HelpButton(tooltip=loss_Fn_tooltip, button_type = "light")
 
 optimizer = Select(title="Optimizer:", value="ADAM", options= optimizer_options, height = 60, width = 300)# Student chooses the optimizer 
-optimizer.on_change('value', status_messages_callback)
+optimizer.on_change('value', train_status_message_callback)
 O_tooltip = Tooltip(content=f"Choose an algorithm by which the neural network will adjust it’s inner neurons. Both choices can be efficient, but may require further tuning of other parameters.", position="left") # {', '.join(optimizer_options)}
 optimizer_help_button = HelpButton(tooltip=O_tooltip, button_type = "light")
 
