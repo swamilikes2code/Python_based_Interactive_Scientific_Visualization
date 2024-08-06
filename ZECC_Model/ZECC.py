fix some scaling issues ZECC bokeh

ZECC_Model/ZECC.py
@@ -15,7 +15,7 @@ time_ranges=["12 Months", "24 Hours"] #possible time ranges
 
 #Creating a map of the world to show where each of the 6 possible locations are
 mapp = figure(x_range=(-14000000, 7000000), y_range=(-4000000, 6060000), # range bounds supplied in web mercator coordinates
-           x_axis_type="mercator", y_axis_type="mercator", aspect_ratio=4/3, height=300, width=400)
+           x_axis_type="mercator", y_axis_type="mercator", aspect_ratio=4/3, height=300, width=380)
 mapp.add_tile("CartoDB Positron", retina=True)
 #adding each location to the map
 mapp.scatter(x=-8389827.854690, y=4957234.168513, size=10, fill_color='blue', fill_alpha=0.7, legend_label="Bethlehem, PA")
@@ -45,7 +45,7 @@ else:
 
 TOOLS = "reset,save,box_zoom" #tools for the graphs
 #Creating Grpah to show average temps throught the year for each location
-diff_temps=figure(title="Average Temperature Throughout the Year", x_axis_label="Months", y_axis_label="Temperature in Celsius", tools=TOOLS, aspect_ratio=4/3, height=300, width=400)
+diff_temps=figure(title="Average Temperature Throughout the Year", x_axis_label="Months", y_axis_label="Temperature in Celsius", tools=TOOLS, aspect_ratio=4/3, height=300, width=380)
 # diff_temps.title.text_font_size='14pt'
 diff_temps.xaxis.ticker = list(range(1, 13))
 diff_temps.xaxis.major_label_overrides={1:'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 
@@ -53,11 +53,11 @@ diff_temps.xaxis.major_label_overrides={1:'January', 2: 'February', 3: 'March', 
 diff_temps.xaxis.major_label_orientation=1
 
 #creating a graph to show the 6 locations temperatures throught one day 
-hourly_temps=figure(title="Temperatures Throughout One Day in Mid-June", x_axis_label="Time in Hours", y_axis_label="Temperature in Celsius", tools=TOOLS, aspect_ratio=4/3, height=300, width=400)
+hourly_temps=figure(title="Temperatures Throughout One Day in Mid-June", x_axis_label="Time in Hours", y_axis_label="Temperature in Celsius", tools=TOOLS, aspect_ratio=4/3, height=300, width=380)
 # hourly_temps.title.text_font_size='14pt'
 
 #Creating a graph to show the average humidity trends for each location throughout the year
-humid=figure(title="Average Humidity Throughout The Year", x_axis_label="Months", y_axis_label="Relative Humidity", x_range=diff_temps.x_range, tools=TOOLS, aspect_ratio=4/3, height=300, width=400)
+humid=figure(title="Average Humidity Throughout The Year", x_axis_label="Months", y_axis_label="Relative Humidity", x_range=diff_temps.x_range, tools=TOOLS, aspect_ratio=4/3, height=300, width=380)
 # humid.title.text_font_size='14pt'
 humid.xaxis.ticker = list(range(1, 13))
 humid.xaxis.major_label_overrides={1:'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 
@@ -117,7 +117,7 @@ start1=np.min(source.data['output'])
 end1=np.max(source.data['output'])
 
 #creating a graph to show the heat conduction and evaporative cooling rate for desired ZECC
-g1=figure(title="Heat per Time", x_axis_label="Time in Months", y_axis_label="Heat Conduction per Time", tools=TOOLS, height=300, width=400)
+g1=figure(title="Heat per Time", x_axis_label="Time in Months", y_axis_label="Heat Conduction per Time", tools=TOOLS, height=300, width=380)
 gg1=g1.line('time', 'output', source=source, color="purple", legend_label="Heat Conduction", line_dash=[4,4], line_width=3)
 g1.y_range=Range1d(start1, end1)
 g1.legend.click_policy="hide"
@@ -130,15 +130,15 @@ for x in range(0,len(yearly_temps_df.index)):
     location_options.append(yearly_temps_df.index[x])
     
 #adding sliders for adjustable dimensions of chamber and drop down menus for location, time interval, and material specifications
-slide_length=Slider(title="Length of Chamber", value=initial_dims[0], start=0, end=12, step=0.5, width=370)
-slide_width=Slider(title="Width of Chamber", value=initial_dims[1], start=0, end=12, step=0.5, width=370)
-slide_height=Slider(title="Height of Chamber", value=initial_dims[2], start=0, end=5, step=0.25, width=370)
-slide_thick=Slider(title="Thickness of Sand Layer in Chamber Wall", value=initial_dims[3], start=0, end=1, step=0.001, width=370)
-select_material=Select(title="Choice of Material for Walls of the Chamber:", value="Brick", options=materials, width=370)
-slide_desired_temp=Slider(title="Desired Temperature for the Inner Chamber", value=20, start=2, end=50, step=0.5, width=370)
-location_select=Select(title="Location", value="Puerto Jiménez, Costa Rica", options=location_options, width=370)
-time_select=Select(title="Time Interval", value="12 Months", options=time_ranges, width=370)
-calculate_button=Button(label="Calculate", button_type='success', width=370) #a button that will calculate cost and water needed when clicked
+slide_length=Slider(title="Length of Chamber", value=initial_dims[0], start=0, end=12, step=0.5, width=350)
+slide_width=Slider(title="Width of Chamber", value=initial_dims[1], start=0, end=12, step=0.5, width=350)
+slide_height=Slider(title="Height of Chamber", value=initial_dims[2], start=0, end=5, step=0.25, width=350)
+slide_thick=Slider(title="Thickness of Sand Layer in Chamber Wall", value=initial_dims[3], start=0, end=1, step=0.001, width=350)
+select_material=Select(title="Choice of Material for Walls of the Chamber:", value="Brick", options=materials, width=350)
+slide_desired_temp=Slider(title="Desired Temperature for the Inner Chamber", value=20, start=2, end=50, step=0.5, width=350)
+location_select=Select(title="Location", value="Puerto Jiménez, Costa Rica", options=location_options, width=350)
+time_select=Select(title="Time Interval", value="12 Months", options=time_ranges, width=350)
+calculate_button=Button(label="Calculate", button_type='success', width=350) #a button that will calculate cost and water needed when clicked
 
 def latent_heat(temp): #function to interpolate latent heat value
     #Interpolating the values for latent heat of evaporation
@@ -300,7 +300,7 @@ sourceTable=ColumnDataSource(data=dict(name=tableName, time=tableTime, Year_Pric
 columnsT=[TableColumn(field='name', title='Location'), TableColumn(field='time', title='Time Interval'), TableColumn(field='space', title='Storage Volume Capacity (in m^3)'), 
           TableColumn(field='Day_Water', title='Daily Water Input (in Liters)'), TableColumn(field='Year_Water', title='Yearly Water Input (in L)'),
           TableColumn(field='Day_Price', title='Daily Cost in $'), TableColumn(field='Year_Price', title='Yearly Cost in $')]
-data_table=DataTable(source=sourceTable, columns=columnsT, width=750)
+data_table=DataTable(source=sourceTable, columns=columnsT, width=380, autosize_mode="none")
 
 def dew_point(temps, rh, time): #calculating dew point of location at speific time
     dp_out=[]